947e3c48c01aa4455dff4e3b7d5047cdf2d79fae
Default <Default@DESKTOP-LCLRU6G.mshome.net>
2020-02-17 17:59:47 +0800

    Search Article Commit

--- Selenium/src/first/WebDriverSelenium.java
+++ Selenium/src/first/WebDriverSelenium.java
@@ -14,7 +14,6 @@ public class WebDriverSelenium {
 	public static void main(String[] args) {
 		WebDriverSelenium webd= new WebDriverSelenium ();
 		webd.openBrower();
-		webd.searchArticle();
 		
 	}
 	public void openBrower() {
@@ -23,16 +22,19 @@ public class WebDriverSelenium {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-		driver.get("http://google.com");
 		
+		searchArticle();
 		
 	}
 	
 	public void searchArticle() {
 		try {
+			driver.get("http://google.com");
 			Thread.sleep(3000);
-			driver.findElement(By.id("q")).sendKeys("Java");
-			driver.findElement(By.id("q")).sendKeys(Keys.ENTER);
+			driver.findElement(By.xpath("/html/body/div/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("Java");
+			driver.findElement(By.xpath("/html/body/div/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys(Keys.ENTER);
+			Thread.sleep(3000);
+			driver.findElement(By.xpath("/html/body/div[7]/div[3]/div[9]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div[1]/a/h3")).click();
 		
 
 		} catch (InterruptedException e) {
